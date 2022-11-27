7c5443c2fc14113484b15f06f825f501365ba47d
Jesus Coloma <colomacardoza16@gmail.com>
2022-11-27 17:48:20 -0500

    firma con responsive

--- Dev/javascript.js
+++ Dev/javascript.js
@@ -1,25 +1,25 @@
 let socials = {
-    facebook: {
-      url: 'https://ed.team/facebook',
-      icon: 'icons/facebook.png'
-    } ,
-    instagram: { 
-      url: 'https://ed.team/twitter',
-      icon: 'icons/instagram.png'
-    },
-    linkedin: {
-      url: 'https://ed.team/linkedin',
-      icon: 'icons/linkedin.png'
-    },
-    github: {
-      url: 'https://ed.team/github',
-      icon: 'icons/github.png'
-    } 
+  facebook: {
+    url: 'https://www.facebook.com/hellaryjesus.colomacardoza',
+    icon: 'https://raw.githubusercontent.com/Jesus-ColomaCardoza/firmaCorreo/master/Dev/icons/facebook.png'
+  },
+  instagram: {
+    url: 'https://www.instagram.com/jesuscolomac/',
+    icon: 'https://raw.githubusercontent.com/Jesus-ColomaCardoza/firmaCorreo/master/Dev/icons/instagram.png'
+  },
+  linkedin: {
+    url: 'https://www.linkedin.com/in/jesuscolomacardoza/',
+    icon: 'https://raw.githubusercontent.com/Jesus-ColomaCardoza/firmaCorreo/master/Dev/icons/linkedin.png'
+  },
+  github: {
+    url: 'https://github.com/Jesus-ColomaCardoza',
+    icon: 'https://raw.githubusercontent.com/Jesus-ColomaCardoza/firmaCorreo/master/Dev/icons/github.png'
   }
-  
-  let socialIcons = '';
-  for(let social in socials) {
-    socialIcons += `<a href="${socials[social].url}" style="display:inline-block;margin-right:15px"><img src="${socials[social].icon}" width="20" style="display: block"></a>`
-  }
-  
-  document.getElementById('sociales').innerHTML = socialIcons;
+}
+
+let socialIcons = '';
+for (let social in socials) {
+  socialIcons += `<a href="${socials[social].url}" style="display:inline-block;margin-right:15px"><img src="${socials[social].icon}" width="20" style="display: block"></a>`
+}
+
+document.getElementById('sociales').innerHTML = socialIcons;
